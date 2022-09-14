fc5f4bfd14b191a4b411d1f3601b4ff0279dccab
thariniK <tharinikndsm@gmail.com>
2022-09-14 23:08:21 +0530

    Upgrading heroku stack

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['JSON_AS_ASCII'] = False
 app.register_blueprint(recipe_api, url_prefix='/v1')
 
 if __name__ == "__main__":  # Makes sure this is the main process
-    app.run( # Starts the site
+    app.run( # Starts the API
         host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
         port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
     )   
